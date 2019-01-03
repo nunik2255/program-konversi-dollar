2de266ac823bb92913bd38398ba6a6c9248f1ed5
Nunik Vigianti <45875868+nunik2255@users.noreply.github.com>
2019-01-03 16:39:55 +0700

    Add files via upload

---
+++ program konversi dollar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    float Rupiah,dollar;
+    cout<<"Program konversi mata uang dollar ke rupiah"<<endl;
+    cout<<"masukan nilai dollar : " ;
+    cin>>dollar;
+    dollar=dollar*15000;
+    cout<<"nilai dollar = "<<dollar<<endl;
+    return 0;
+}
